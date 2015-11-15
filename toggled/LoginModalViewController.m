a6b48d70bac82b992f3c61e0fc99bac0afb7a1cf
Seth Rylan Gainey <seth.rylan@gmail.com>
2015-11-15 12:58:07 -0500

    Added notes.

--- toggled/LoginModalViewController.m
+++ toggled/LoginModalViewController.m
@@ -2,6 +2,9 @@
 //  Created by Seth Rylan Gainey on 10/14/15.
 //  Copyright (c) 2015 Seth Rylan Gainey. All rights reserved.
 //
+//  See http://stackoverflow.com/questions/16230700/display-uiviewcontroller-as-popup-in-iphone for development hints
+//
+
 
 #import "LoginModalViewController.h"
 
